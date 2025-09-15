5a74b33dfd29a85ae4d1057d4963148a1edaae74
joker <97875782+Joelroys637@users.noreply.github.com>
2025-09-15 07:11:21 +0530

    Update settings.py

--- mongo/settings.py
+++ mongo/settings.py
@@ -128,10 +128,13 @@ MIDDLEWARE = [
 ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
